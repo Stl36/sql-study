5708fe843b408ff3dc097992b71bd14fb9aac3fb
Aleksey Shmykov <shmykov.job@gmail.com>
2021-05-23 21:29:29 +0300

    Update shmykovan_lesson10.sql

--- lesson_10/shmykovan_lesson10.sql
+++ lesson_10/shmykovan_lesson10.sql
@@ -1,5 +1,12 @@
 -- 1.Проанализировать какие запросы могут выполняться наиболее часто в процессе работы приложения и добавить необходимые индексы.
 
+-- ответ
+/*
+ ответ будет зависеть от того, как используется БД в продашене под нагрузкой. Но стоит не забывать, что на столбцы первичных ключей , внешних ключей, а также столбцы на которых определено ограничение уникальности индексы строятся автоматически самой СУБД. Поэтому одиночные индексы на эти стоблцы не нужно строить
+Пример ниже
+ */
+SELECT * FROM ;
+
 
 /*
 2.Задание на оконные функции
@@ -12,3 +19,21 @@
  -- всего пользователей в системе (количество пользователей в таблице users)
  -- отношение в процентах для последних двух значений (общее количество пользователей в группе / всего пользователей в системе) * 100
 */
+
+  
+SELECT DISTINCT 
+  communities.name AS group_name,
+  COUNT(communities_users.user_id) OVER() / (SELECT COUNT(*) FROM communities) AS avg_users_in_groups,
+  FIRST_VALUE(CONCAT(users.first_name, ' ', users.last_name)) OVER (w_community ORDER BY profiles.birthday DESC) AS must_young,
+  FIRST_VALUE(CONCAT(users.first_name, ' ', users.last_name)) OVER (w_community ORDER BY profiles.birthday ASC) AS must_old,
+  COUNT(communities_users.user_id) OVER w_community AS users_in_group, 
+  (SELECT COUNT(*) FROM users) AS users_total,
+  COUNT(communities_users.user_id) OVER w_community / (SELECT COUNT(*) FROM users) *100 AS '%%'
+    FROM communities
+      LEFT JOIN communities_users 
+        ON communities_users.community_id = communities.id
+      LEFT JOIN users 
+        ON communities_users.user_id = users.id
+      LEFT JOIN profiles 
+        ON profiles.user_id = users.id
+      WINDOW w_community AS (PARTITION BY communities.id);  
